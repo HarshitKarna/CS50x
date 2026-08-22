4ff20907e61ceb6b97251d9c5fbfcb66c6a267de
Harshit Karna <harshitkarna64@gmail.com>
2026-08-22 18:07:32 +0545

    Added problem set 2. Solutions yet to be submitted

---
+++ Week 2/Problem Set 2/caesar_l_comfortable.c
@@ -0,0 +1,17 @@
+/*
+Basic Caesar cipher. Accept a positive number by the user when calling out the program, use that number as the secret key. If user didnt give a number, say "Usage: {program_name} {key}" ;and also set the program return code to 1.
+
+Can only rotate between alphabets, cant go into the special symbols territory.
+Output must be in the format of:
+plaintext:
+ciphertext:
+
+NOTE: preserve capitalization
+*/
+
+#include<stdio.h>
+
+int main(int argc, int argv[])
+{
+    
+}
---
+++ Week 2/Problem Set 2/substitution_m_comfortable.c
@@ -0,0 +1,15 @@
+/*
+Same rules as caesar_l_comfortable, but accept a 26 character key from the user and replace the corresponding ABC... with the given key.
+
+accept each letter only once.
+the given key is assumed to correspond to normal lower-case letters.
+IF the plaintext has capital letters, turn its equivalent "key" letter into the reverse of what was entered.
+i.e. say Hello was entered and the corresponding key of hello is KSbUN. then, since H was actually capital, reverse K to be k instead.
+*/
+
+#include<stdio.h>
+
+int main(int argc, int argv[])
+{
+    
+}
