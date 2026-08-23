1843f249dc15899fa16fde209b01913431b4b888
Harshit Karna <harshitkarna64@gmail.com>
2026-08-23 14:46:38 +0545

    Added caesar cipher problem solution

--- Week 2/Problem Set 2/caesar_l_comfortable.c
+++ Week 2/Problem Set 2/caesar_l_comfortable.c
@@ -9,9 +9,42 @@ ciphertext:
 NOTE: preserve capitalization
 */
 
+#include<ctype.h>
 #include<stdio.h>
+#include<string.h>
+#include "../cs50.c"
+#include "../cs50.h"
 
-int main(int argc, int argv[])
+int main(int argc, char *argv[])
 {
-    
+    if(argc != 2)
+    {
+        printf("Usage: caesar_l_comfortable {some number}");
+        return 1;
+    }
+
+    int secret_key = (atoi(argv[1])) % 26;
+    printf("Plaintext: ");
+    string plaintext = GetString();
+    char ciphertext[strlen(plaintext)+1];
+
+    for(int i = 0; plaintext[i] != '\0'; i++)
+    {
+        if(plaintext[i] >= 'a' && plaintext[i] <= 'z')
+        {
+            ciphertext[i] = 'a' + ((plaintext[i] - 'a' + secret_key) % 26);
+        }
+        else if(plaintext[i] >= 'A' && plaintext[i] <= 'Z')
+        {
+            ciphertext[i] = 'A' + ((plaintext[i] - 'A' + secret_key) % 26);
+        }
+        else
+        {
+            ciphertext[i] = plaintext[i];
+        }
+    }
+
+    ciphertext[strlen(plaintext)] = '\0'; // why does C not do this automatically????
+    printf("Ciphertext: %s", ciphertext);
+    return 0;
 }
