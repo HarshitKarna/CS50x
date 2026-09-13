e921b36f22d15939d2342188778600f668c91680
Harshit Karna <harshitkarna64@gmail.com>
2026-09-13 21:16:04 +0545

    Added birthdays solution

--- Week 9/Problem Set 9/birthdays/app.py
+++ Week 9/Problem Set 9/birthdays/app.py
@@ -1,7 +1,5 @@
-import os
-
-from cs50 import SQL
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
+import sqlite3
+from flask import Flask, redirect, render_template, request
 
 # Configure application
 app = Flask(__name__)
@@ -9,8 +7,11 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///birthdays.db")
+# Configure birthdays.db using sqlite3; creates new if doesnt exist
+def get_db():
+    db = sqlite3.connect("birthdays.db")
+    db.row_factory = sqlite3.Row
+    return db
 
 
 @app.after_request
@@ -24,16 +25,49 @@ def after_request(response):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    birthdays_conn = get_db()
+    birthdays_cursor = birthdays_conn.cursor()
     if request.method == "POST":
 
-        # TODO: Add the user's entry into the database
+        # Add the user's entry into the database
+        person_name = request.form.get("name")
+        bday_month = int(request.form.get("month"))
+        bday_day = int(request.form.get("day"))
+        
+        if bday_month <= 0 or bday_month > 12:
+            return render_template("error.html", message="Invalid Month")
+        
+        if bday_day <= 0 or bday_day > 31:
+            return render_template("error.html", message="Invalid Day")
+        
+        birthdays_cursor.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", (person_name, bday_month, bday_day))
+        
+        birthdays_conn.commit()
+        birthdays_conn.close()
 
         return redirect("/")
 
     else:
 
-        # TODO: Display the entries in the database on index.html
+        # Display the entries in the database on index.html
+        birthdays_cursor.execute("SELECT * FROM birthdays")
+        people = birthdays_cursor.fetchall()
+        
+        birthdays_conn.close()
 
-        return render_template("index.html")
+        return render_template("index.html", people=people)
 
 
+@app.route("/delete", methods = ["POST"])
+def delete():
+    birthdays_conn = get_db()
+    birthdays_cursor = birthdays_conn.cursor()
+
+    person_id = request.form.get("id")
+
+    birthdays_cursor.execute("DELETE FROM birthdays WHERE id = ?", (person_id,))
+
+    birthdays_conn.commit()
+    birthdays_conn.close()
+
+    return redirect("/")
